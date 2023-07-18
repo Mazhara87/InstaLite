7acf1fb079a5e338ea02d1a3162f0a6fad0d2f8f
Diana0806 <diananovak871@gmail.com>
2023-07-18 10:27:10 +0200

    add index, profil

--- process/add-users.php
+++ process/add-users.php
@@ -1,15 +1,15 @@
 <?php
 
-
+session_start();
 require_once('../connexion.php');
 
 // var_dump($_POST);
 
-if(
-    isset($_POST['username']) && !empty($_POST['username'])
-    &&isset($_POST['password']) && !empty($_POST['password'])
-    &&isset($_POST['name']) && !empty($_POST['name'])
-){
+if (
+    !empty($_POST['username'])
+    && !empty($_POST['password'])
+    && !empty($_POST['name'])
+) {
     $data = [
         'username' => $_POST['username'],
         'password' => $_POST['password'],
@@ -17,8 +17,12 @@ if(
 
     ];
     $sql = "INSERT INTO users (username, password, name) VALUES (:username, :password, :name)";
-    $request= $db->prepare($sql);
+    $request = $db->prepare($sql);
     $request->execute($data);
+
+    $_SESSION['user']['id'] = $db->lastInsertId();
+    $_SESSION['user']['username'] = $_POST['username'];
+    $_SESSION['user']['password'] =  $_POST['password'];
+    $_SESSION['user']['name'] =  $_POST['name'];
 }
-header('Location: profil-user.php')
-?>
+header('Location: ../profil-user.php');
--- profil-user.php
+++ profil-user.php
@@ -1,6 +1,8 @@
 <?php
-require_once('./connexion.php');
+session_start();
+require_once('connexion.php');
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -11,36 +13,8 @@ require_once('./connexion.php');
 </head>
 <body>
     <h1>My Insta</h1>
+    <p><strong>Name:</strong> <?php echo $_SESSION['user']['name']; ?></p>
 
-    <?php
 
-  
-   
-    if (isset($_GET['id']) && is_numeric($_GET['id'])) {
-        
-
-        
-        $query = "SELECT * FROM users WHERE id = :id";
-        $request = $db->prepare($query);
-        $request->bindParam(':id', $_GET['id'], PDO::PARAM_INT);
-        $request->execute();
-
-        if ($request->rowCount() > 0) {
-            $user = $request->fetch(PDO::FETCH_ASSOC);
-            echo "<p><strong>Username :</strong> " . $user['username'] . "</p>";
-            echo "<p><strong>Password :</strong> " . $user['password'] . "</p>";
-            echo "<p><strong>Name :</strong> " . $user['name'] . "</p>";
-
-            //   // Add the link to the update page
-            //   echo "<a href='update-avatar.php?id=" . $user['id'] . "'>Change avatar</a>";
-            
-        } else {
-            echo "User not found.";
-        }
-    } else {
-        echo "Invalid user ID.";
-    }
-
-    ?>
 </body>
 </html>
